8cc2e0cc572ccc77998da0055882c54d24c17c43
Nitheshkumar <iamnk1433@gmail.com>
2021-07-22 06:29:58 +0000

    Calculate function with mutation.

--- index.js
+++ index.js
@@ -5,32 +5,14 @@ console.log(JSON.stringify(data,null,'\t'))
 
 const calculateCost = (task) => {
     let childCost = (task.cost || 0);
-    task.tasks?.forEach((item) => {
-        childCost += calculateCost(item);
-    });
+    task.tasks?.forEach((item) => childCost += calculateCost(item));
     task.cost = childCost;
-    console.log(childCost)
     return childCost;
-}
-
-const actions = {
-    cost: (data) => {
-        let totalCost = (data.cost || 0);
-        data.tasks?.forEach((item) => {
-            totalCost += calculateCost(item);
-            data.cost = totalCost;
-        });
-        return totalCost;
-    },
 };
 
-const keywords = Object.keys(actions);
+calculateCost(data);
 
-const parser = (data) => {
-    Object.keys(data).filter((item) => keywords.indexOf(item) !== -1).
-    forEach((key) => actions[key](data));
-};
-parser(data)
+
 console.log(JSON.stringify(data,null,'\t'))
-// console.log(data)
+
 
