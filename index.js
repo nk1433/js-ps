f0e01be6d29f25bf7f696b38898c3bfb7a54c4d5
Nitheshkumar <iamnk1433@gmail.com>
2021-07-22 06:39:33 +0000

    Refactored output.

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const {task} = require("mocks");
 
 const data = task();
-console.log(JSON.stringify(data,null,'\t'))
+console.log(JSON.stringify(data,null,'\t'));
 
 
 const calculateCostWithoutMutate = (task) => {
     let output = {};
+    let tasks = [];
     let totalCost = (task.cost || 0);
-    output.tasks = task.tasks?.map((item) => {
+    tasks = task.tasks?.map((item) => {
         let task = calculateCostWithoutMutate(item);
         totalCost += task.cost;
         return task;
     })
     output.cost = totalCost;
+    output.tasks = tasks;
     return output;
 }
 
 const output = calculateCostWithoutMutate(data);
 
-console.log(JSON.stringify(output,null,'\t'))
+console.log(JSON.stringify(output,null,'\t'));
 
 
